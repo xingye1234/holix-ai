3b47afd5e74ee37076e24a6037ae52d0c1df2d4b
zhaogongchegsi <zzh1586169624@163.com>
2026-01-06 09:48:45 +0800

    添加系统提示

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,24 +1,29 @@
 declare global {
   interface ImportMetaEnv {
-    readonly NODE_ENV: 'development' | 'production' | 'test'
-    readonly BASE_URL: string
-    readonly DEV: boolean
-    readonly PROD: boolean
+    readonly NODE_ENV: "development" | "production" | "test";
+    readonly BASE_URL: string;
+    readonly DEV: boolean;
+    readonly PROD: boolean;
   }
 
   interface ImportMeta {
-    readonly env: ImportMetaEnv
+    readonly env: ImportMetaEnv;
   }
 }
 
-declare module '*.svg' {
-  const content: string
-  export default content
+declare module "*.svg" {
+  const content: string;
+  export default content;
 }
 
-declare module '*.png' {
-  const content: string
-  export default content
+declare module "*.png" {
+  const content: string;
+  export default content;
 }
 
-export {}
+declare module "*.md" {
+  const content: string;
+  export default content;
+}
+
+export {};
