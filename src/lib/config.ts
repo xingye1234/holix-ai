a0349c8a1dd21f2de114f4ea61f5035ac29bafd2
zhaogongchengsi <zzh1586169624@163.com>
2026-01-19 23:14:52 +0800

    fix: 统一代码风格，调整缩进和格式

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,11 +1,10 @@
-import type { ConfigData } from "@/node/platform/config";
-import { kyInstance } from "./ky";
-
+import type { ConfigData } from '@/node/platform/config'
+import { kyInstance } from './ky'
 
 export async function getConfig() {
-	return await kyInstance.get('config').json<ConfigData>()
+  return await kyInstance.get('config').json<ConfigData>()
 }
 
 export async function updateConfig<K extends keyof ConfigData>(key: K, value: ConfigData[K]) {
-	return await kyInstance.post('config', { json: { key, value } }).json<ConfigData>()
+  return await kyInstance.post('config', { json: { key, value } }).json<ConfigData>()
 }
