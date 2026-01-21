6db7f757287ee715f1154c234405eda8dd24a178
zhaogongchegsi <zzh1586169624@163.com>
2026-01-21 09:58:10 +0800

    feat: 统一代码缩进风格，提升可读性

--- src/node/database/connect.ts
+++ src/node/database/connect.ts
@@ -1,37 +1,38 @@
+import { resolve } from 'node:path'
 import process from 'node:process'
 import { migrate } from 'drizzle-orm/libsql/migrator'
 import { drizzle } from 'drizzle-orm/libsql/sqlite3'
 import { app } from 'electron'
-import { resolve } from 'path'
 import { databaseUrl } from '../constant'
 
 const promiser = Promise.withResolvers<void>()
 
 export const db = drizzle({
-	connection: databaseUrl,
+  connection: databaseUrl,
 })
 
 let hasMigrated = false
 export async function migrateDb() {
-	if (hasMigrated) {
-		return
-	}
-	const dir = resolve(app.isPackaged ? process.resourcesPath : process.cwd(), 'drizzle')
-	try {
-		await migrate(db, { migrationsFolder: dir })
-		hasMigrated = true
-		promiser.resolve()
-	} catch (err) {
-		promiser.reject(err)
-		throw err
-	}
+  if (hasMigrated) {
+    return
+  }
+  const dir = resolve(app.isPackaged ? process.resourcesPath : process.cwd(), 'drizzle')
+  try {
+    await migrate(db, { migrationsFolder: dir })
+    hasMigrated = true
+    promiser.resolve()
+  }
+  catch (err) {
+    promiser.reject(err)
+    throw err
+  }
 }
 
 export async function waitForDbMigrations() {
-	return promiser.promise
+  return promiser.promise
 }
 
 export async function getDatabase() {
-	await waitForDbMigrations()
-	return db
+  await waitForDbMigrations()
+  return db
 }
