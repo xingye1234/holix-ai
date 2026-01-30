1cb0f289ce1d6f633d8a503efc003d1102cd85dd
zhaogongchegsi <zzh1586169624@163.com>
2026-01-30 12:20:58 +0800

    feat: 将 Command 类型从 type 重构为 interface，简化类型定义

--- src/types/commands/base.ts
+++ src/types/commands/base.ts
@@ -1,8 +1,6 @@
-
-
-export type Command<N = string, P extends Record<string, unknown> = Record<string, unknown>> = {
-	id: string;
-	name: N;
-	timestamp: number;
-	payload: P;
+export interface Command<N = string, P extends Record<string, unknown> = Record<string, unknown>> {
+  id: string
+  name: N
+  timestamp: number
+  payload: P
 }
