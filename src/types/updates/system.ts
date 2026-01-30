f4d672c268c44373b4255fbc71a8f244f5e3ba0e
zhaogongchegsi <zzh1586169624@163.com>
2026-01-30 15:21:31 +0800

    feat: 移除窗口更新类型中的空负载，简化类型定义

--- src/types/updates/system.ts
+++ src/types/updates/system.ts
@@ -1,7 +1,7 @@
 import type { EventEnvelope } from './base'
 
 // 窗口最小化更新（无负载）
-export type WindowMinimizeEnvelope = EventEnvelope<'window.minimize', {}>
+export type WindowMinimizeEnvelope = EventEnvelope<'window.minimize'>
 
 // 窗口最大化/还原更新，payload 表示当前是否为最大化
 export type WindowMaximizeEnvelope = EventEnvelope<
@@ -10,7 +10,7 @@ export type WindowMaximizeEnvelope = EventEnvelope<
 >
 
 // 窗口关闭更新（无负载）
-export type WindowCloseEnvelope = EventEnvelope<'window.close', {}>
+export type WindowCloseEnvelope = EventEnvelope<'window.close'>
 
 // 可选：将三者合并为一个联合类型
 export type WindowUpdateEnvelope
