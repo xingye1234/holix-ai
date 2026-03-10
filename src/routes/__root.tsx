b9c9df206783b9bb723eacd28516f2227c9dddcc
Buzhuzhao <64842422+zhaogongchengsi@users.noreply.github.com>
2026-03-10 10:41:20 +0800

    feat(skills): add simple skill store page and link sidebar entry

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,7 +2,6 @@ import { createRootRoute, Outlet } from '@tanstack/react-router'
 import AppHeader from '@/components/app/app-header'
 import AppMain from '@/components/app/app-main'
 import AppSideBar from '@/components/app/app-sidebar'
-import { Separator } from '@/components/ui/separator'
 import { AsideChatSidebar } from '@/views/chat/chat'
 import { AsideChatHeader } from '@/views/chat/header'
 
@@ -14,7 +13,6 @@ function RootLayout() {
         <section className="flex border-t h-[calc(100vh-var(--app-header-height))]">
           <AppSideBar>
             <AsideChatHeader />
-            <Separator />
             <AsideChatSidebar />
           </AppSideBar>
           <AppMain>
